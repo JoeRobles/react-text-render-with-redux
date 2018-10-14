59506bba3d2794d1c3adf8a7fee66e2e748a359c
Joe Robles <joe.robles.pdj@gmail.com>
2018-10-13 23:18:16 -0500

    mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 
+import { action } from './actions/text.action';
+
 class App extends Component {
   render() {
-    console.log('props: ', this.props);
     return (
       <div></div>
     );
@@ -15,4 +16,8 @@ const mapStateToProps = state => ({
   state: state.text
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = {
+  onUpdateText: action
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
